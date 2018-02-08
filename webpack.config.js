33cc05e7d7ece9eb338ebdb771144eb9105d374b
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-02-08 08:09:59 +0200

    prettify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,81 +6,65 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var inProduction = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-
 	entry: {
 		app: [
-		'./src/app.js',
-		'./src/app.scss'
+			'./src/app.js',
+			'./src/app.scss'
 		]
 	},
-
 	output: {
 		path: path.resolve(__dirname, './dist'),
 		filename: '[name].js'
 	},
-
 	devtool: !inProduction && "source-map",
-
 	module: {
-		rules: [
-
-		{
-			test: /\.s[ac]ss$/,
-			use: ExtractTextPlugin.extract({
-				use: [
-				{
-					loader: 'css-loader', options: {
-						sourceMap: !inProduction
-					}
-				},
-				{
-					loader: 'sass-loader', options: {
-						sourceMap: !inProduction
-					}
-				}
-				],
-				fallback: 'style-loader'
-			})
-		},
-
-		{
-			test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-			loader: 'file-loader?name=fonts/[name].[ext]'
-		},
-
-		{ 
-			test: /\.js$/, 
-			exclude: /node_modules/, 
-			loader: "babel-loader"
-		}
-
+		rules: [{
+				test: /\.s[ac]ss$/,
+				use: ExtractTextPlugin.extract({
+					use: [{
+							loader: 'css-loader',
+							options: {
+								sourceMap: !inProduction
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: !inProduction
+							}
+						}
+					],
+					fallback: 'style-loader'
+				})
+			},
+			{
+				test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+				loader: 'file-loader?name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
 		]
 	},
-
 	plugins: [
-
-	new ExtractTextPlugin("[name].css"),
-
-	new webpack.LoaderOptionsPlugin({
-		minimize: inProduction
-	}),
-
-	new HtmlWebpackPlugin({
-		template: 'src/index.html'
-	}),
-
+		new ExtractTextPlugin("[name].css"),
+		new webpack.LoaderOptionsPlugin({
+			minimize: inProduction
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		}),
 	],
-
 	devServer: {
 		contentBase: path.join(__dirname, "dist"),
 		compress: true,
 		port: 9000
 	},
-
 };
-
 if (inProduction) {
 	module.exports.plugins.push(
 		new webpack.optimize.UglifyJsPlugin()
-		)
+	)
 }

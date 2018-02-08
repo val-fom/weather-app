feb632338d9156aac2a21c4e114ba5686bec18c3
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-02-08 08:44:20 +0200

    add Weather class

---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import { get } from './api'
+
+export class Weather {
+	constructor(city) {
+		this.apiType = 'weather';
+		this.units = 'metric'; //default
+		this.city = 'kyiv'; //default
+		this.responseWeather = null;
+		this.responseForecast = null;
+	}
+
+	getWeather() {
+		get(this.apiType, this.units, this.city)
+			.then(data => {
+				if (this.apiType === 'weather') {
+					this.responseWeather = data;
+				} else if (this.apiType === 'forecast') {
+					this.responseForecast = data;
+				}
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	}
+
+}

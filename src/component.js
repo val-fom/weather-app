5be61934190ced2f065bf96054e2e7d6434e3efc
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-02-12 14:30:23 +0200

    add getIcons method to Component class

--- src/component.js
+++ src/component.js
@@ -1,5 +1,19 @@
 export class Component {
 	constructor() {
-		
+
+	}
+
+	getIcons(source) {
+		let icons = '';
+		for (var i = source.length - 1; i >= 0; i--) {
+			// source[i] /// weather
+			let timeOfDay = ''; // to set -day or -night icon type
+			if (source[i].icon.endsWith('n')) timeOfDay = '-night';
+			if (source[i].icon.endsWith('d')) timeOfDay = '-day';
+			const icon = `<i class="wi wi-owm${timeOfDay}-${source[i].id}"` +
+				`title="${source[i].main}: ${source[i].description}"></i>`;
+			icons += icon;
+		}
+		return icons;
 	}
 }

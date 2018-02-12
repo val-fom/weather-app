27badb7b539be9b8aaddbc100cc062e05b461651
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-02-13 01:03:25 +0200

    show the sun/moon only when it's clean or partly cloudy

--- src/component.js
+++ src/component.js
@@ -7,8 +7,10 @@ export class Component {
 		let icons = '';
 		for (var i = source.length - 1; i >= 0; i--) {
 			let timeOfDay = '';
-			if (source[i].icon.endsWith('n')) timeOfDay = '-night';
-			if (source[i].icon.endsWith('d')) timeOfDay = '-day';
+			if (source[i].id >= 800 && source[i].id <= 803) {
+				if (source[i].icon.endsWith('n')) timeOfDay = '-night';
+				if (source[i].icon.endsWith('d')) timeOfDay = '-day';
+			}
 			icons += `<i class="wi wi-owm${timeOfDay}-${source[i].id}"` +
 				`title="${source[i].main}: ${source[i].description}"></i>`;
 		}

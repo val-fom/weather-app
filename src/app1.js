67503191f17f49707146c5893ccb8db0a04dfb26
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-02-26 18:28:43 +0200

    cleanup

--- src/app1.js
+++ src/app1.js
@@ -8,40 +8,8 @@ export class App1 {
 		this.responseForecast = null;
 	}
 
-	getAll(city) {
-		return this._getWeather('weather', city)
-			.then(() => this._getWeather('forecast'))
-	}
-
-	_getWeather(apiType, city) {
-		if (!city) city = this.city;
-		return get(apiType, units.value, city)
-			.then(data => {
-				if (apiType === 'weather') {
-					this.responseWeather = data;
-					this._setCityConfig();
-					this._updateUrl();
-					this._setCityTitle();
-				} else if (apiType === 'forecast') {
-					this.responseForecast = data;
-				}
-			})
-	}
-
-	// _getCityFromUrl() {
-	// 	let url = new URL(window.location.href);
-	// 	if (url.search.startsWith('?q=')) {
-	// 		return url.search.slice(3);
-	// 	}
-	// }
-
 	_updateUrl() {
 		window.history.pushState(null, null, `?q=${this.city}`);
-	}
-
-	_setCityConfig() {
-		this.city = this.responseWeather.name +
-			',' + this.responseWeather.sys.country;
 	}
 
 	_setCityTitle() {
